ab848e008ab49d3ef4305f476f50266aa0c57d3e
Gueyefall <gpape89@gmail.com>
2024-03-15 15:58:02 +0100

    Deployment script for realtime on specified camera

--- realtime_detection.py
+++ realtime_detection.py
@@ -10,7 +10,12 @@ print(next(model.parameters()).is_cuda)
 
 ''' The recommended approach is to use stream=True when calling model.predict() to avoid this issue.'''
 # Predict using a video file with streaming to avoid RAM accumulation
-results = model.predict(source="1", device="cuda", save=False, show=True, stream=True, show_conf=False)
+results = model.predict(source="1", 
+                        device="cuda", 
+                        save=False, 
+                        show=True, 
+                        stream=True, 
+                        show_conf=False)
 
 # Now, when performing inference or training, check if the model is on GPU
 print(next(model.parameters()).is_cuda)
